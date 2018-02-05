8f41e8a6b81e6c5df4e8f8254d4da56bbcb9d4a5
Thomas Michiels <mi_thom@hotmail.com>
2018-02-05 03:48:11 +0100

    improve class_lvls relation

--- app/controllers/authenticated/characters_controller.rb
+++ app/controllers/authenticated/characters_controller.rb
@@ -3,6 +3,8 @@ module Authenticated
 
     def index
       authorize Character
+      # TODO: fix n+1 problem here, problem with embedded eager loading
+      # @characters = current_user.characters.includes(:character_class, from: :class_lvls)
       @characters = current_user.characters
       skip_policy_scope  #if we have admins, we can expand this to other users etc.
     end
--- app/helpers/authenticated/characters_helper.rb
+++ app/helpers/authenticated/characters_helper.rb
@@ -2,7 +2,7 @@ module Authenticated
   module CharactersHelper
 
     def class_lvl_string(class_lvl)
-     class_lvl.character_class.pluck(:name).first + ': ' + class_lvl.lvl.to_s
+     class_lvl.character_class.name + ': ' + class_lvl.lvl.to_s
     end
 
     def class_lvl_strings(character)
@@ -10,8 +10,7 @@ module Authenticated
     end
 
     def classes_string(character)
-      #de classes worden hier verder niet ingeladen, dus pluck is efficiënter
-      character.class_lvls.all.collect { |class_lvl| class_lvl.character_class.pluck(:name).first }.join(' + ')
+      character.class_lvls.all.collect { |class_lvl| class_lvl.character_class.name }.join(' + ')
     end
 
   end
